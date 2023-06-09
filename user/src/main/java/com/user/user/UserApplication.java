e31bde8ba8222d6167dc7c7d17df61aba5450fab
dengchaojun <dengchaojun@tian028.com>
2023-06-09 17:13:12 +0800

    add:Module user

---
+++ user/src/main/java/com/user/user/UserApplication.java
@@ -0,0 +1,21 @@
+package com.user.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 用户应用程序
+ *
+ * @author S
+ * @date 2023/06/09
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
---
+++ user/src/main/java/com/user/user/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.user.user.controller;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : DCJ
+ * @date : 2023-06-09
+ **/
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/1")
+    public void run (){
+        System.out.println("x");
+    }
+}
