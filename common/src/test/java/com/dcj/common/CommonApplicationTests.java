a83fa31cdccbb453c4a78b9d6b07351934932820
dengchaojun <dengchaojun@tian028.com>
2023-07-17 10:16:48 +0800

    add:TS

---
+++ common/src/test/java/com/dcj/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcj.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
